bf30ef66adc4cff8b8acd105034f3bd80a897ac4
MShiva939 <mdshiva125@gmail.com>
2025-08-08 09:18:26 +0530

    updated

--- terraform/s3/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-sre1-project"
-    key            = "s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"   # Optional, for state locking
-    encrypt        = true
-  }
-}
