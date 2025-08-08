b5c59ca2ce532a06ced636198fc71755fdc469c5
MShiva939 <mdshiva125@gmail.com>
2025-08-08 08:52:53 +0530

    Updated main.tf

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -14,40 +14,4 @@ provider "aws" {
   region = var.region
 }
 
-# # --- S3 Bucket ---
-# resource "aws_s3_bucket" "tf_state" {
-#   bucket = var.backend_bucket
-# }
 
-# resource "aws_s3_bucket_acl" "tf_state_acl" {
-#   bucket = aws_s3_bucket.tf_state.id
-#   acl    = "private"
-# }
-
-# resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-#   bucket = aws_s3_bucket.tf_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
-# resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
-#   bucket = aws_s3_bucket.tf_state.id
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
-
-# # --- DynamoDB Table ---
-# resource "aws_dynamodb_table" "tf_locks" {
-#   name         = var.dynamodb_table
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
